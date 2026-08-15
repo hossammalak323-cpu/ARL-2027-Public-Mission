7a9b2ec6c8994ae9499fcf99babe64fd938735bf
hossammalak323-cpu <hossammalak323@gmail.com>
2026-08-16 01:45:45 +0300

    Update Question.py

--- Coding_Assignment/Question.py
+++ Coding_Assignment/Question.py
@@ -12,4 +12,25 @@ def monitor_following_distance(distances: list[float], speeds: list[float]) -> t
             - minimum_distance: absolute closest distance to the lead car (return 0.0 if empty list)
             - tailgate_incidents: number of separate instances the car started tailgating
     """
-    pass
+    tailgating_seconds=0
+    tailgate_incidents=0
+    previous_tailgating=False
+    if distances:
+        minimum_distance=min(distances)
+    else:
+        minimum_distance= 0.0
+        
+    for i in range(len(distances)):
+        safe_distance=speeds[i]*0.5
+        is_tailgating= distances[i] < safe_distance
+        
+        if is_tailgating:
+            tailgating_seconds+= 1 
+        
+        if is_tailgating and not previous_tailgating:
+            tailgate_incidents+= 1
+        previous_tailgating=is_tailgating
+
+    return tailgating_seconds, minimum_distance, tailgate_incidents
+      
+        
